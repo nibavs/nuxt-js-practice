8e3903e83a1a82a0400cd87e99f0c5d49c424844
Nikita Bavs <nbavsunovsky@gmail.com>
2025-10-29 19:23:24 +0700

    routing are done

---
+++ pages/users/[user_id].vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1>User id: {{ user_id }}</h1>
+    </div>
+</template>
+
+<script setup lang="ts">
+const { user_id } = useRoute().params;
+</script>
+
+<style scoped>
+
+</style>
